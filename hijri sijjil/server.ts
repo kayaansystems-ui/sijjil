4eda5eb4b08ed7e3c7540d642e1c5fd87d1747e7
kayaansystems-ui <kayaansystems@gmail.com>
2026-07-07 10:25:23 +0900

    Update server.ts

--- hijri sijjil/server.ts
+++ hijri sijjil/server.ts
@@ -229,7 +229,7 @@ const LOCAL_NAME_DATABASE: Record<string, any> = {
 
 async function startServer() {
   const app = express();
-  const PORT = 3000;
+  const PORT = Number(process.env.PORT) || 3000;
 
   app.use(express.json());
 
